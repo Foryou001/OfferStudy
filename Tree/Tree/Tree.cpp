a0972423012ada10d969293d02dbf9a4fd4965cc
符逸 <foryou@hrbeu.edu.cn>
2019-02-25 13:29:09 +0800

    add main function

---
+++ Tree/Tree/Tree.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct BinaryTreeNode
+{
+	int m_nValue;
+	BinaryTreeNode* m_pLeft;
+	BinaryTreeNode* m_pRight;
+};
+
+void RebuildBinaryTree()
+{
+	vector<int> vecPre;
+	vector<int> vecMid;
+
+	cout << "Please input the PreOrder:\n";
+	int tmp;
+	cin >> tmp;
+	vecPre.push_back(tmp);
+	while (cin.get() != '\n')
+	{
+		cin >> tmp;
+		vecPre.push_back(tmp);
+
+	}
+
+	cout << "Please input hte MidOrder:\n";
+	cin >> tmp;
+	vecMid.push_back(tmp);
+	while (cin.get() != '\n')
+	{
+		cin >> tmp;
+		vecMid.push_back(tmp);
+
+	}
+
+
+
+	
+}
