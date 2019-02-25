ecba315302cc04ffaa24bb610196427af875e1c2
ForYou <foryou@hrbeu.edu.cn>
2019-02-25 10:01:56 +0800

    recover

---
+++ OneWayLinkedList/OneWayLinkedList.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+//OneWayLinkedList Definition
+
+
+
+struct ListNode
+{
+	int m_nValue;
+	ListNode* m_pNext;
+
+};
+
+
+//print the LinkedList
+
+void PrintAllNode(ListNode** pHead)
+{
+	ListNode* p = *pHead;
+	while (p->m_pNext != nullptr)
+	{
+		cout << p->m_nValue << " ";
+		p = p->m_pNext;
+	}
+	cout << p->m_nValue <<endl;
+}
+
+//Add a node To the tail
+
+void AddToTail(ListNode** pHead, int value)
+{
+	ListNode* pNew = new ListNode();
+	pNew->m_nValue = value;
+	pNew->m_pNext = nullptr;
+
+	if (*pHead == nullptr)
+	{
+		*pHead = pNew;
+	}
+	else
+	{
+		//find the old tail
+		ListNode* pNode = *pHead;
+
+		while (pNode->m_pNext != nullptr)
+		{
+			pNode = pNode->m_pNext;
+		}
+
+		//add the new tail after the old one
+		pNode->m_pNext = pNew;
+
+	}
+}
+
+
+int main()
+{
+	ListNode* OneWayLinkedList = nullptr;
+	int add_value;
+	cin >> add_value;
+	AddToTail(&OneWayLinkedList, add_value);
+	while (cin.get() != '\n')
+	{
+		cin >> add_value;
+		AddToTail(&OneWayLinkedList, add_value);
+	}
+
+	PrintAllNode(&OneWayLinkedList);
+
+	system("pause");
+
+	return 0;
+
+}
