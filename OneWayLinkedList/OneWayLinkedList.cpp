f8df113b14e5a84b7925e3528e484c649fdeebb6
符逸 <foryou@hrbeu.edu.cn>
2019-02-25 09:41:56 +0800

    Revert "完成了单向链表的逆向打印"
    This reverts commit 4777fa47c3b4c6226052b4880f861f3572f9f305.

--- OneWayLinkedList/OneWayLinkedList.cpp
+++ OneWayLinkedList/OneWayLinkedList.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <vector>
-#include <stack>
+
 using namespace std;
 
 
@@ -23,20 +22,16 @@ struct ListNode
 void PrintAllNode(ListNode** pHead)
 {
 	ListNode* p = *pHead;
-	while (p!= nullptr)
+	while (p->m_pNext != nullptr)
 	{
 		cout << p->m_nValue << " ";
 		p = p->m_pNext;
 	}
-	cout << endl;
+	cout << p->m_nValue <<endl;
 }
 
 //Add a node To the tail
-/*为什么要将函数的第一个参数pHead设为指向指针的指针。
-因为当我们往一个空链表中插入一个节点时，新插入的节点就是
-链表的头指针。由于此时会改动头指针，因此必须把pHead参数
-设置为指向指针的指针，否则出了这个函数pHead仍然是一个空指针
-*/
+
 void AddToTail(ListNode** pHead, int value)
 {
 	ListNode* pNew = new ListNode();
@@ -63,100 +58,7 @@ void AddToTail(ListNode** pHead, int value)
 	}
 }
 
-//Remove the Node
 
-void RemoveNode(ListNode** pHead, int value)
-{
-	if (pHead == nullptr || *pHead == nullptr)
-	{
-		return;
-	}
-
-
-
-	ListNode* pDeleteBefore = *pHead;
-	ListNode* pDelete = pDeleteBefore->m_pNext;
-
-	if (pDeleteBefore->m_nValue == value)
-	{
-		*pHead = pDelete;
-		delete *pHead;
-		*pHead = nullptr;
-		return;
-	}
-
-	while (pDeleteBefore->m_pNext != nullptr)
-	{
-		pDelete = pDeleteBefore->m_pNext;
-
-		if (pDelete->m_nValue == value)
-		{
-			pDeleteBefore->m_pNext = pDelete->m_pNext;
-			delete pDelete;
-			pDelete = nullptr;
-			break;
-		}
-
-		pDeleteBefore = pDeleteBefore->m_pNext;
-
-	}
-}
-
-
-//从尾到头打印链表
-void ReversePrint(ListNode* pHead)
-{
-
-	if (pHead == nullptr)
-	{
-		return;
-	}
-
-	ListNode* pNode = pHead;
-	vector<int> vecValue;
-	int value;
-	while (pNode->m_pNext != nullptr)
-	{
-		value = pNode->m_nValue;
-		vecValue.push_back(value);
-		pNode = pNode->m_pNext;
-	}
-	value = pNode->m_nValue;
-	vecValue.push_back(value);
-	vector<int>::iterator it;
-	for (it = vecValue.end() - 1; it != vecValue.begin(); --it)
-	{
-		cout << *it << " ";
-	}
-	cout << *it << endl;
-}
-
-
-
-//use stack
-
-void PrintListReversingly_Iteratively(ListNode* pHead)
-{
-	stack<ListNode*> nodes;
-
-	ListNode* pNode = pHead;
-
-	while (pNode != nullptr)
-	{
-		nodes.push(pNode);
-		pNode = pNode->m_pNext;
-
-	}
-
-	while (!nodes.empty())
-	{
-		pNode = nodes.top();
-		cout << pNode->m_nValue << "\t";
-		nodes.pop();
-
-	}
-	
-}
 int main()
 {
 	ListNode* OneWayLinkedList = nullptr;
@@ -169,20 +71,7 @@ int main()
 		AddToTail(&OneWayLinkedList, add_value);
 	}
 
-	//PrintAllNode(&OneWayLinkedList);
-
-	//input the node value to delete
-	/*int value_delete;
-	cin >> value_delete;
-
-	RemoveNode(&OneWayLinkedList, value_delete);
-
-	*/
 	PrintAllNode(&OneWayLinkedList);
-
-	ReversePrint(OneWayLinkedList);
-
-	PrintListReversingly_Iteratively(OneWayLinkedList);
 
 	system("pause");
 
